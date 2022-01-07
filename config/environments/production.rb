7df0929540f18b28579c1f93a5f47a23070e7217
MexpressoBean <kingk102@gmail.com>
2022-01-06 23:49:27 -0800

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "social_stock_trading_app_production"
 
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: 'https://social-stock-trades-6.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
