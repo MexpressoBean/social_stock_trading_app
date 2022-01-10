0db17c322c1387b7a4a830f5dc5999ec7d3945e8
MexpressoBean <kingk102@gmail.com>
2022-01-09 22:31:43 -0800

    added ability for user to remove a stock from their list

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,7 +6,15 @@ class UserStocksController < ApplicationController
             stock.save
         end
         @user_stock = UserStock.create(user: current_user, stock: stock)
-        flash[:notice] = "Stock #{stock.name} successfully added"
+        flash[:notice] = "Stock #{stock.ticker} successfully added"
+        redirect_to my_portfolio_path
+    end
+
+    def destroy
+        stock = Stock.find(params[:id])
+        user_stock = UserStock.where(user_id: current_user, stock_id: stock.id).first
+        user_stock.destroy
+        flash[:notice] = "Stock #{stock.ticker} successfully removed"
         redirect_to my_portfolio_path
     end
 end
