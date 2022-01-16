ac67e9a070ad606218e0d014babb86db20fb88a7
MexpressoBean <kingk102@gmail.com>
2022-01-15 20:06:20 -0800

    added code for unfollow button

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :friendships, only: [:create, :destroy]
 end
