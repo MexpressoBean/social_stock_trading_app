a356110a20a0dc67c3b7b165fd63360b24a64394
MexpressoBean <kingk102@gmail.com>
2022-01-15 20:48:25 -0800

    implemented unfollow button

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,19 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])  
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "Now following #{friend.first_name}"
+    else
+      flash[:notice] = "Not following, something went wrong."
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
-    flash.now[:notice] = 'Unfollowed successfully'
+    flash[:notice] = 'Unfollowed'
     redirect_to my_friends_path
   end
 end
